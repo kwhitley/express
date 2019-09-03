b34abec33e920f0315441830dbd0c3924f8263ce
Kevin Whitley <krwhitley@gmail.com>
2019-09-02 21:14:15 -0500

    released v0.3.0 - adding NODE_ENV to client-var injection

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,12 +1,29 @@
 // returns only process.env prefixed with CLIENT_{something}
-export const getClientEnvironmentVariables = () =>
-  Object
-    .keys(process && process.env || {})
+export const getClientEnvironmentVariables = (options = {}) => {
+  if (!process || !process.env) {
+    return {}
+  }
+
+  const varsToExpose = ['NODE_ENV']
+
+  const clientEnvironmentVariables = Object.keys(process.env)
     .filter(v => v.indexOf('CLIENT_') === 0)
     .reduce((a, key) => {
       a[key] = process.env[key]
       return a
     }, {})
+
+  // any vars explicitly exposed will get added here
+  varsToExpose.forEach(key => {
+    const value = process.env[key]
+
+    if (value !== undefined) {
+      clientEnvironmentVariables[key] = value
+    }
+  })
+
+  return clientEnvironmentVariables
+}
 
 export const configInjection = `
       const ENV_KEY = window.ENV_KEY = Symbol.for('ENV')
@@ -14,7 +31,7 @@ export const configInjection = `
       // all public ENV vars injected here
       window[ENV_KEY] = ${JSON.stringify(getClientEnvironmentVariables() || {}, null, 2)}
 
-      // getter for public vars (e.g. getEnv('CLIENT_AUTH0_DOMAIN'))
+      // getter for public vars (e.g. getEnv('CLIENT_ROOT_URL'))
       const getEnv = window.getEnv = (key = '', options = {}) => {
         let { required } = options
         let value = window[ENV_KEY][key]
